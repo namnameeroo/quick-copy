ec5cc41ae2f3c94846d2eb794c0713b299fceca1
namnameeroo <nami.skageec@gmail.com>
2024-03-25 17:52:24 +0900

    env: router setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import ThemesPage from "./pages/ThemesPage";
 
 function App() {
-  return (
-    <div className="App">
-      <ThemesPage />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/utils/router.jsx
@@ -0,0 +1,7 @@
+import ThemesPage from "../pages/ThemesPage";
+
+export const paths = [
+  {path: "/", element: <></>},
+  {path: "/theme-page", element: <ThemesPage />},
+  // {path: "/theme-page", element: <BoardPage />},
+];
